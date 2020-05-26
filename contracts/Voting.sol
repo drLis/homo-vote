0c228b70ffd2ba9a78941a489a8933457228e210
dr.lis <dr.lis@outlook.com>
2020-05-27 02:15:16 +0300

    Contract completed

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,19 +2,20 @@ pragma solidity >=0.6.0;
 
 contract Voting
 {
-	event Voted(address voter, address indexed candidate);
+	event Voted(address voter, uint[4] voteVector);
 
 	mapping (address => bool) public voted;
 
-	mapping (address => uint) public votesForCandidate;
+	mapping (uint => uint) public summaryVoteVector;
 
-	function vote(address candidate) public
+	function vote(uint[4] memory voteVector) public
 	{
 		require(voted[msg.sender] == false, "You must vote only one times!");
 
-		votesForCandidate[candidate]++;
+		for (uint i = 0; i < 4; i++)
+			summaryVoteVector[i] += voteVector[i];
 		voted[msg.sender] = true;
 
-		emit Voted(msg.sender, candidate);
+		emit Voted(msg.sender, voteVector);
 	}
 }
