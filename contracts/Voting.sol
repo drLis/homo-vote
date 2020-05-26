070377f185cd6419317604ceaad03407b1398717
dr.lis <dr.lis@outlook.com>
2020-05-26 13:18:07 +0300

    Simpe vote completed

---
+++ contracts/Voting.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.6.0;
+
+contract Voting
+{
+	event Voted(address voter, uint candidate);
+
+	mapping (address => bool) public voted;
+	
+	uint[4] public votesForCandidate;
+
+	function vote(uint candidate) public
+	{
+		require(voted[msg.sender] == false, "You must vote only one times!");
+
+		votesForCandidate[candidate]++;
+		voted[msg.sender] = true;
+
+		emit Voted(msg.sender, candidate)
+	}
+}
