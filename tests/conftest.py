2799da979f748acddaa8699e7cf1fb591b9b6daf
dr.lis <dr.lis@outlook.com>
2020-05-27 02:17:39 +0300

    Fixture completed

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.fixture(scope="function", autouse=True)
+def isolate(fn_isolation):
+    pass
+
+@pytest.fixture(scope="module")
+def voting(Voting, accounts):
+    return accounts[0].deploy(Voting)
