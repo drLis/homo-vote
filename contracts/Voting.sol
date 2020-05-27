a8d6ea8f5c86551b69fed5170e398b2782c32e31
dr.lis <dr.lis@outlook.com>
2020-05-27 07:21:30 +0300

    Voting woth encryption completed

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -6,13 +6,19 @@ contract Voting
 {
 	using HomomorphicHiding for E;
 
-	event Voted(address voter, uint[4] voteVector);
+	event Voted(address voter);
 
 	uint constant public n = 10;
 
 	mapping (address => bool) public voted;
 
 	mapping (uint => E) public summaryVoteVector;
+
+	constructor() public
+	{
+		for (uint i = 0; i < 4; i++)
+			summaryVoteVector[i] = HomomorphicHiding.e(0);
+	}
 
 	function vote(uint[2][4] memory input) public
 	{
@@ -26,10 +32,10 @@ contract Voting
 		require(voteVector[0].add(voteVector[1]).add(voteVector[2]).add(voteVector[3]).equals(HomomorphicHiding.e(n)), "Sum of shares must be whole.");
 
 		for (uint i = 0; i < 4; i++)
-			summaryVoteVector[i] += voteVector[i];
+			summaryVoteVector[i] = summaryVoteVector[i].add(voteVector[i]);
 		voted[msg.sender] = true;
 
-		emit Voted(msg.sender, voteVector);
+		emit Voted(msg.sender);
 	}
 
 	function readE(uint[2] memory input) internal pure returns (E memory e)
