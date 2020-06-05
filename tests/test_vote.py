67b57a68cbab01791216ab590be6f28ded986d3a
dr.lis <dr.lis@outlook.com>
2020-06-05 21:33:18 +0300

    Completed test

--- tests/test_vote.py
+++ tests/test_vote.py
@@ -1,7 +1,15 @@
 import pytest
 import brownie
 
+
 def test_vote(voting, accounts):
+
+	def e(vector):
+		new_vector = []
+		for i in vector:
+			new_vector.append(voting.e(i))
+		return new_vector
+
 	alice = accounts[0]
 	bob = accounts[1]
 	carl = accounts[2]
@@ -10,11 +18,13 @@ def test_vote(voting, accounts):
 	bob_vector = [1, 1, 5, 3]
 	carl_vector = [1, 1, 0, 8]
 
-	tx1 = voting.vote(alice_vector, {"from": alice})
-	tx2 = voting.vote(bob_vector, {"from": bob})
-	tx3 = voting.vote(carl_vector, {"from": carl})
+	tx1 = voting.vote(e(alice_vector), {"from": alice})
+	tx2 = voting.vote(e(bob_vector), {"from": bob})
+	tx3 = voting.vote(e(carl_vector), {"from": carl})
 
-	assert voting.summaryVoteVector(0) == 5
-	assert voting.summaryVoteVector(1) == 7
-	assert voting.summaryVoteVector(2) == 6
-	assert voting.summaryVoteVector(3) == 12
+	assert voting.summaryVoteVector(0) == voting.e(5)
+	assert voting.summaryVoteVector(1) == voting.e(7)
+	assert voting.summaryVoteVector(2) == voting.e(6)
+	assert voting.summaryVoteVector(3) == voting.e(12)
+
+	assert voting.check([5, 7, 6, 12]) == True
